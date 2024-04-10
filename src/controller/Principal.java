a4b25f4c2f1f3cc4ff40b78f0d7bc541ffadbde3
Santiago <sanmanmedper2020@gmail.com>
2024-04-10 12:54:59 +0200

    fix: Controlador Principal
    Arreglos para cuando ejecutes te redirija a la página principal.

--- src/controller/Principal.java
+++ src/controller/Principal.java
@@ -1,10 +1,15 @@
 package controller;
 
+import view.Main;
+
 public class Principal {
 	
 	public static void main(String[] args) {
 		// Llamamos a la ventana principal desde la vista
-		
+        Main ventanaPrincipal = new Main();
+        
+        // Hacemos visible la ventana principal
+        ventanaPrincipal.setVisible(true);
 	}
 	
 }
