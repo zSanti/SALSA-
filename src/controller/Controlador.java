70d171c9dd3fc3098344d848e1aeb0e7fee54846
Santiago Manuel <80847631+zSanti@users.noreply.github.com>
2024-04-19 00:38:59 +0200

    fix: Controlador
    Esto creo lo puedo eliminar no lo utilizo, ya se verá mañana

--- src/controller/Controlador.java
+++ src/controller/Controlador.java
@@ -4,17 +4,12 @@ import clases.Persona;
 import model.ImpleDB;
 
 public class Controlador {
-	private Dao dao = new ImpleDB();
 	
-	public boolean iniciarSesion(String nombreUsuario, String contrasena) {
-		return false;
-	}
+	// creo puedo borrar esto :v
 
 	public static boolean registrarUsuario(Persona per) {
 		Dao dao = new ImpleDB();
-
 		return dao.registrarUsuario(per);
-
 	}
 
 }
