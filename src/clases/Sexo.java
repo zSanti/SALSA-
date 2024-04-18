530d3fb270a7c9a66db3fc034acf8a9f59a0a9f0
lucia9928 <144560946+lucia9928@users.noreply.github.com>
2024-04-18 12:42:12 +0200

    Delete src directory

--- src/clases/Sexo.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public enum Sexo {
-	MASCULINO, FEMENINO, OTROS
-}
--- src/controller/Principal.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class Principal {
-
-	public static void main(String[] args) {
-
-	}
-
-}
