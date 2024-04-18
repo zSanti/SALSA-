8b1bc07c3b90b0f5e53ad4df21507956b89818fa
Santiago Manuel <80847631+zSanti@users.noreply.github.com>
2024-04-19 00:39:22 +0200

    fix: Principal Controller
    Paso de DAO como parámetro

--- src/controller/Principal.java
+++ src/controller/Principal.java
@@ -5,8 +5,9 @@ import view.Main;
 public class Principal {
 	
 	public static void main(String[] args) {
+		Dao dao = null;
 		// Llamamos a la ventana principal desde la vista
-        Main ventanaPrincipal = new Main();
+        Main ventanaPrincipal = new Main(dao);
         
         // Hacemos visible la ventana principal
         ventanaPrincipal.setVisible(true);
