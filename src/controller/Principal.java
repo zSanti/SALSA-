3ff90fee862827d497cd696ef39cd169214667d3
Santiago <sanmanmedper2020@gmail.com>
2024-04-19 09:44:10 +0200

    fix: Principal Controller
    Cambio en la ventana principal, en vez de como se ve la web, lo paso por el login.

--- src/controller/Principal.java
+++ src/controller/Principal.java
@@ -1,13 +1,13 @@
 package controller;
 
-import view.Main;
+import view.Login;
 
 public class Principal {
 	
 	public static void main(String[] args) {
 		Dao dao = null;
 		// Llamamos a la ventana principal desde la vista
-        Main ventanaPrincipal = new Main(dao);
+        Login ventanaPrincipal = new Login(dao);
         
         // Hacemos visible la ventana principal
         ventanaPrincipal.setVisible(true);
