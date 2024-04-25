6cee38203256e278e1f68605b60233fbd9b1728c
Santiago <sanmanmedper2020@gmail.com>
2024-04-25 11:53:51 +0200

    fix: Cambio en el inicio del programa con los parametros correctos

--- src/controller/Principal.java
+++ src/controller/Principal.java
@@ -1,15 +1,21 @@
 package controller;
 
+import clases.Persona;
 import view.Login;
 
 public class Principal {
 
 	public static void main(String[] args) {
-
-		Controlador cont = new Controlador();
+		// Instanciamos y creamos un objeto para el controlador y para la persona
+		Controlador controladorRutas = new Controlador();
+		Persona persona = new Persona();
+		
+		// Definimos que el modo sea oscuro
 		boolean oscuro = false;
-		Login ven = new Login(cont, oscuro);
-		ven.setVisible(true);
+		
+		// Le pasamos la ventana principal que en nuestro caso es el Login
+		Login login = new Login(controladorRutas, persona, oscuro);
+		login.setVisible(true);
 
 	}
 
